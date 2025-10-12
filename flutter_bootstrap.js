96c7d871c617ff187308e21149417a23e27b947a
Ahmad_Ali <ahmadalirj99@gmail.com>
2025-10-12 19:49:57 +0500

    Built web
    Branch: main
    Commit: 64747e4c2260deb4f19691aa066329d1c0d83516
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "549063885"
+    serviceWorkerVersion: "409099665"
   }
 });
